b431b080b034cde47982a21fbd0f13d1ebb30508
TommasoBernardo <tommasob1210@gmail.com>
2023-03-01 17:55:30 +0100

    add slug

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,9 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'accent_color', 'bg_color', 'slug',];
 
     public function posts()
     {
         return $this->belongsToMany(Post::class);
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
